4dbe1b2c3e330671b4ac9c9405cb7b3bdef4498c
Anton K <anton@prilet.com>
2010-10-09 16:48:00 -0700

    add ssmtp recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -29,7 +29,7 @@ require_recipe "delayed_job"
 #require_recipe "eybackup_slave"
 
 #uncomment to run the ssmtp recipe
-#require_recipe "ssmtp"
+require_recipe "ssmtp"
 
 #uncomment to run the mongodb recipe
 # require_recipe "mongodb"
