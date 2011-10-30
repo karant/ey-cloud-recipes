811e047b04652cb9e75167fb6030559c20aa2009
Anton K <anton@prilet.com>
2011-10-29 21:28:03 -0700

    reverse the symlink change to sphinx recipe

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -81,11 +81,7 @@ if utility_name
         owner node[:owner_name]
         group node[:owner_name]
         mode 0755
-      end
-      
-      link "/data/#{app_name}/shared/config/sphinx" do
-        to "/data/#{app_name}/current/config/sphinx"
-      end      
+      end    
 
       remote_file "/etc/logrotate.d/sphinx" do
         owner "root"
